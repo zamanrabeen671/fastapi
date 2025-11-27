3111e9b5c0a255e614000e82170624100678bf0f
KAMRUZZAMAN RABEEN <46568226+zamanrabeen671@users.noreply.github.com>
2025-11-27 12:42:11 +0600

    init db

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,  Depends, HTTPException
 from models import Product
 from database import SessionLocal, engine
 import database_models
-
+from sqlalchemy.orm import Session
 app = FastAPI()
 database_models.Base.metadata.create_all(bind=engine)
 
@@ -17,19 +17,38 @@ products = [
     Product(id = 4, name = "mobile cp", description= "this is smartphone", price = 100.11,  quantity=2),
 ]
 
-@app.get('/products')
-def product():
+def get_db():
     db = SessionLocal()
-    db.query()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def _init_db():
+    db = SessionLocal()
+
+    existing_count = db.query(database_models.Product).count()
+
+    if existing_count == 0:
+        for product in products:
+            db.add(database_models.Product(**product.model_dump()))
+        db.commit()
+        print("Database initialized with sample products.")
+        
+    db.close()
+_init_db()
+
+@app.get("/products/")
+def get_all_products(db: Session = Depends(get_db)):
+    products = db.query(database_models.Product).all()
     return products
 
-@app.get('/product/{id}')
-def singleProduct(id: int):
-    for product in products:
-        if product.id == id:
-            return product
-    
-    return "Not Found"
+@app.get("/products/{product_id}")
+def get_product_by_id(product_id: int, db: Session = Depends(get_db)):
+    product = db.query(database_models.Product).filter(database_models.Product.id == product_id).first()
+    if product:
+        return product
+    return {"error": "Product not found"}
 
 @app.post('/product')
 def add_product(product: Product):
